f6783a12421c29830cc67b89e713696c314aeca8
yaotutu <957810668@qq.com>
2023-07-22 23:32:25 +0800

    .

--- src/components/MyRCFieldForm/useForm.jsx
+++ src/components/MyRCFieldForm/useForm.jsx
@@ -1,13 +1,41 @@
+import { useRef } from "react"
 
 class FormStore {
   
+  constructor(){
+    this.store = {}
+  }
+
+  getFiledsValue = () => {
+    return this.store
+  }
+  getFiledValue = (name) => {
+    return this.store[name]
+  }
+  setFailedValue = (newStore) => {
+    this.store = {
+      ...this.store,
+      ...newStore
+    }
+    console.log('this.store:', this.store)
+  }
+  getForm = ( ) => {
+    return {
+      getFiledValue : this.getFiledValue,
+      getFiledsValue: this.getFiledsValue,
+      setFailedValue: this.setFailedValue
+    }
+  }
 }
+
 const useForm = () => {
-  return (
-    <div>
-      useForm 
-    </div>
-  )
+  // 把数据存在ref，也就是组件的fiber身上，这样组件更新的时候，form的数据可以保存下来
+  const formRef = useRef()
+  if(!formRef.current){
+    const formStore = new FormStore()
+    formRef.current = formStore
+  }
+  return []
 }
 
 export default useForm
